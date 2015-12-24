b06f011c8d4ef8a1cf8300482da837c42c56af84
Martin McNulty <martin.mcnulty@gsacapital.com>
2015-12-24 00:26:48 +0000

    Finish day 10

--- day10.scala
+++ day10.scala
@@ -5,11 +5,20 @@ object Main {
   def main(args: Array[String]) {
     var phrase = io.Source.fromFile(args(0)).getLines.next
     1 to 40 foreach { _ => phrase = lookAndSay(phrase) }
-    println(phrase)
+    println(phrase.length)
+    41 to 50 foreach { _ => phrase = lookAndSay(phrase) }
+    println(phrase.length)
   }
 
   def lookAndSay(in: String): String = {
-    ???
+    @annotation.tailrec
+    def _say(cs: List[Char], acc: List[Char]): List[Char] = cs match {
+      case Nil => acc.reverse
+      case a :: _ =>
+        val (run, rest) = cs.span(_ == a)
+        _say(rest, a :: run.length.toString.toList ++ acc)
+    }
+    _say(in.toList, Nil) mkString ""
   }
 
 }
