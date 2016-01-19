7d1749e63b9c3a459bcdc1a47aa5428325a0f213
Martin McNulty <martin@mcnulty.me.uk>
2016-01-19 20:21:32 +0000

    Add compile and run script for *nix

---
+++ compile-and-run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCALA_HOME=~/scala/2.11.7
+DAY=$1
+echo Compiling day $DAY...
+$SCALA_HOME/bin/scalac -d bin day${DAY}.scala
+echo Running day $DAY...
+$SCALA_HOME/bin/scala -classpath bin advent.day${DAY}.Main day${DAY}-input.txt
